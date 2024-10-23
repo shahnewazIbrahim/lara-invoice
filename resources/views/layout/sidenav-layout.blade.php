460b7d6a9df626283b2f544722c5a75937bc331c
shahnewazIbrahim <shahnewaz886@gmail.com>
2024-10-23 15:40:51 +0600

    active class add

--- resources/views/layout/sidenav-layout.blade.php
+++ resources/views/layout/sidenav-layout.blade.php
@@ -1,29 +1,31 @@
 <!DOCTYPE html>
 <html lang="en" data-bs-theme="light">
+
 <head>
-    <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <title></title>
 
-    <link rel="icon" type="image/x-icon" href="{{asset('/favicon.ico')}}" />
-    <link href="{{asset('css/bootstrap.css')}}" rel="stylesheet" />
-    <link href="{{asset('css/animate.min.css')}}" rel="stylesheet" />
-    <link href="{{asset('css/fontawesome.css')}}" rel="stylesheet" />
-    <link href="{{asset('css/style.css')}}" rel="stylesheet" />
-    <link href="{{asset('css/toastify.min.css')}}" rel="stylesheet" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('/favicon.ico') }}" />
+    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/animate.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/fontawesome.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/toastify.min.css') }}" rel="stylesheet" />
 
 
-    <link href="{{asset('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css')}}" rel="stylesheet" />
+    <link href="{{ asset('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css') }}"
+        rel="stylesheet" />
 
-    <link href="{{asset('css/jquery.dataTables.min.css')}}" rel="stylesheet" />
-    <script src="{{asset('js/jquery-3.7.0.min.js')}}"></script>
-    <script src="{{asset('js/jquery.dataTables.min.js')}}"></script>
+    <link href="{{ asset('css/jquery.dataTables.min.css') }}" rel="stylesheet" />
+    <script src="{{ asset('js/jquery-3.7.0.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.dataTables.min.js') }}"></script>
 
 
-    <script src="{{asset('js/toastify-js.js')}}"></script>
-    <script src="{{asset('js/axios.min.js')}}"></script>
-    <script src="{{asset('js/config.js')}}"></script>
-    <script src="{{asset('js/bootstrap.bundle.js')}}"></script>
+    <script src="{{ asset('js/toastify-js.js') }}"></script>
+    <script src="{{ asset('js/axios.min.js') }}"></script>
+    <script src="{{ asset('js/config.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.bundle.js') }}"></script>
 
 
 
@@ -32,108 +34,115 @@
 
 <body>
 
-<div id="loader" class="LoadingOverlay d-none">
-    <div class="Line-Progress">
-        <div class="indeterminate"></div>
+    <div id="loader" class="LoadingOverlay d-none">
+        <div class="Line-Progress">
+            <div class="indeterminate"></div>
+        </div>
     </div>
-</div>
 
-<nav class="navbar fixed-top px-0 shadow-sm bg-white">
-    <div class="container-fluid">
+    <nav class="navbar fixed-top px-0 shadow-sm bg-white">
+        <div class="container-fluid">
 
-        <a class="navbar-brand" href="#">
-            <span class="icon-nav m-0 h5" onclick="MenuBarClickHandler()">
-                <img class="nav-logo-sm mx-2"  src="{{asset('images/menu.svg')}}" alt="logo"/>
-            </span>
-            <img class="nav-logo  mx-2"  src="{{asset('images/logo.png')}}" alt="logo"/>
-        </a>
+            <a class="navbar-brand" href="#">
+                <span class="icon-nav m-0 h5" onclick="MenuBarClickHandler()">
+                    <img class="nav-logo-sm mx-2" src="{{ asset('images/menu.svg') }}" alt="logo" />
+                </span>
+                <img class="nav-logo  mx-2" src="{{ asset('images/logo.png') }}" alt="logo" />
+            </a>
 
-        <div class="float-right h-auto d-flex">
-            <div class="user-dropdown">
-                <img class="icon-nav-img" src="{{asset('images/user.webp')}}" alt=""/>
-                <div class="user-dropdown-content ">
-                    <div class="mt-4 text-center">
-                        <img class="icon-nav-img" src="{{asset('images/user.webp')}}" alt=""/>
-                        <h6>User Name</h6>
-                        <hr class="user-dropdown-divider  p-0"/>
+            <div class="float-right h-auto d-flex">
+                <div class="user-dropdown">
+                    <img class="icon-nav-img" src="{{ asset('images/user.webp') }}" alt="" />
+                    <div class="user-dropdown-content ">
+                        <div class="mt-4 text-center">
+                            <img class="icon-nav-img" src="{{ asset('images/user.webp') }}" alt="" />
+                            <h6>User Name</h6>
+                            <hr class="user-dropdown-divider  p-0" />
+                        </div>
+                        <a href="{{ url('/user-rofile') }}" class="side-bar-item">
+                            <span class="side-bar-item-caption">Profile</span>
+                        </a>
+                        <a href="{{ url('/logout') }}" class="side-bar-item">
+                            <span class="side-bar-item-caption">Logout</span>
+                        </a>
                     </div>
-                    <a href="{{url('/user-rofile')}}" class="side-bar-item">
-                        <span class="side-bar-item-caption">Profile</span>
-                    </a>
-                    <a href="{{url("/logout")}}" class="side-bar-item">
-                        <span class="side-bar-item-caption">Logout</span>
-                    </a>
                 </div>
             </div>
         </div>
+    </nav>
+
+
+    <div id="sideNavRef" class="side-nav-open">
+        <a href="{{ url('/dashboard') }}"
+            class="side-bar-item {{ request()->segment(1) === 'dashboard' ? 'side-bar-item-active' : '' }}">
+            <i class="bi bi-graph-up"></i>
+            <span class="side-bar-item-caption">Dashboard</span>
+        </a>
+
+        <a href="{{ url('/customer-page') }}"
+            class="side-bar-item {{ request()->segment(1) === 'customer-page' ? 'side-bar-item-active' : '' }}">
+            <i class="bi bi-people"></i>
+            <span class="side-bar-item-caption">Customer</span>
+        </a>
+
+        <a href="{{ url('/category-page') }}"
+            class="side-bar-item {{ request()->segment(1) === 'category-page' ? 'side-bar-item-active' : '' }}">
+            <i class="bi bi-list-nested"></i>
+            <span class="side-bar-item-caption">Category</span>
+        </a>
+
+        <a href="{{ url('/product-page') }}"
+            class="side-bar-item {{ request()->segment(1) === 'product-page' ? 'side-bar-item-active' : '' }}">
+            <i class="bi bi-bag"></i>
+            <span class="side-bar-item-caption">Product</span>
+        </a>
+
+        <a href="{{ url('/sale-page') }}"
+            class="side-bar-item {{ request()->segment(1) === 'sale-page' ? 'side-bar-item-active' : '' }}">
+            <i class="bi bi-currency-dollar"></i>
+            <span class="side-bar-item-caption">Create Sale</span>
+        </a>
+
+        <a href="{{ url('/invoice-page') }}"
+            class="side-bar-item {{ request()->segment(1) === 'invoice-page' ? 'side-bar-item-active' : '' }}">
+            <i class="bi bi-receipt"></i>
+            <span class="side-bar-item-caption">Invoice</span>
+        </a>
+
+        <a href="{{ url('/report-page') }}"
+            class="side-bar-item {{ request()->segment(1) === 'report-page' ? 'side-bar-item-active' : '' }}">
+            <i class="bi bi-file-earmark-bar-graph"></i>
+            <span class="side-bar-item-caption">Report</span>
+        </a>
+
+
     </div>
-</nav>
 
 
-<div id="sideNavRef" class="side-nav-open">
-
-    <a href="{{url("/dashboard")}}" class="side-bar-item">
-        <i class="bi bi-graph-up"></i>
-        <span class="side-bar-item-caption">Dashboard</span>
-    </a>
-
-    <a href="{{url("/customer-page")}}" class="side-bar-item">
-        <i class="bi bi-people"></i>
-        <span class="side-bar-item-caption">Customer</span>
-    </a>
-
-    <a href="{{url("/category-page")}}" class="side-bar-item">
-        <i class="bi bi-list-nested"></i>
-        <span class="side-bar-item-caption">Category</span>
-    </a>
-
-    <a href="{{url("/product-page")}}" class="side-bar-item">
-        <i class="bi bi-bag"></i>
-        <span class="side-bar-item-caption">Product</span>
-    </a>
-
-    <a href="{{url('/sale-page')}}" class="side-bar-item">
-        <i class="bi bi-currency-dollar"></i>
-        <span class="side-bar-item-caption">Create Sale</span>
-    </a>
-
-    <a href="{{url('/invoice-page')}}" class="side-bar-item">
-        <i class="bi bi-receipt"></i>
-        <span class="side-bar-item-caption">Invoice</span>
-    </a>
-
-    <a href="{{url('/report-page')}}" class="side-bar-item">
-        <i class="bi bi-file-earmark-bar-graph"></i>
-        <span class="side-bar-item-caption">Report</span>
-    </a>
-
-
-</div>
-
-
-<div id="contentRef" class="content">
-    @yield('content')
-</div>
+    <div id="contentRef" class="content">
+        @yield('content')
+    </div>
 
 
 
-<script>
-    function MenuBarClickHandler() {
-        let sideNav = document.getElementById('sideNavRef');
-        let content = document.getElementById('contentRef');
-        if (sideNav.classList.contains("side-nav-open")) {
-            sideNav.classList.add("side-nav-close");
-            sideNav.classList.remove("side-nav-open");
-            content.classList.add("content-expand");
-            content.classList.remove("content");
-        } else {
-            sideNav.classList.remove("side-nav-close");
-            sideNav.classList.add("side-nav-open");
-            content.classList.remove("content-expand");
-            content.classList.add("content");
+    <script>
+        function MenuBarClickHandler() {
+            let sideNav = document.getElementById('sideNavRef');
+            let content = document.getElementById('contentRef');
+            if (sideNav.classList.contains("side-nav-open")) {
+                sideNav.classList.add("side-nav-close");
+                sideNav.classList.remove("side-nav-open");
+                content.classList.add("content-expand");
+                content.classList.remove("content");
+            } else {
+                sideNav.classList.remove("side-nav-close");
+                sideNav.classList.add("side-nav-open");
+                content.classList.remove("content-expand");
+                content.classList.add("content");
+            }
         }
-    }
-</script>
+    </script>
 
 </body>
+
 </html>
